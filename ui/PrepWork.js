8126e092835d4837ddadc6413d904b47cf460154
Matthew Hatcher <hello@matthewh.in>
2016-03-13 18:33:45 -0400

    PrepWork rewrite.

--- ui/PrepWork.js
+++ ui/PrepWork.js
@@ -1,21 +1,13 @@
-var spawn = require('child_process').spawn;
-var React = spawn('cmd.exe', ['/c', 'browserify -t [ babelify --presets [ react ] ] browserify/main.js -o src/react/App.js']);
-var Script = spawn('cmd.exe', ['/c', 'browserify browserify/site.js | uglifyjs > src/assets/js/main.js']);
-React.stdout.on('data', function (data) {
-  console.log(data.toString());
-});
-React.stderr.on('data', function (data) {
-  console.log(data.toString());
-});
-React.on('exit', function (code) {
+var exec = require('child_process').exec;
+exec('browserify -t [ babelify --presets [ react ] ] browserify/main.js -o src/react/App.js', function (err, stdout, stderr) {
+  if (err) {
+    throw err;
+  }
   console.log('ReactJS built.');
 });
-Script.stdout.on('data', function (data) {
-  console.log(data.toString());
-});
-Script.stderr.on('data', function (data) {
-  console.log(data.toString());
-});
-Script.on('exit', function (code) {
+exec('browserify browserify/site.js | uglifyjs > src/assets/js/main.js', function (err, stdout, stderr) {
+  if (err) {
+    throw err;
+  }
   console.log('Site scripts built.');
 });
