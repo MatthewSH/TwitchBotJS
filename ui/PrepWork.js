cd2552d1775f74ce2c6507644d77bb5f8ba3cdba
Matthew Hatcher <hello@matthewh.in>
2016-03-13 14:45:33 -0400

    Updating prep work.

--- ui/PrepWork.js
+++ ui/PrepWork.js
@@ -1,21 +1,21 @@
-const spawn = require('child_process').spawn;
-const React = spawn('cmd.exe', ['/c', 'browserify -t [ babelify --presets [ react ] ] browserify/main.js -o src/react/App.js']);
-const Script = spawn('cmd.exe', ['/c', 'browserify browserify/site.js -o src/assets/js/main.js']);
-React.stdout.on('data', (data) => {
+var spawn = require('child_process').spawn;
+var React = spawn('cmd.exe', ['/c', 'browserify -t [ babelify --presets [ react ] ] browserify/main.js -o src/react/App.js']);
+var Script = spawn('cmd.exe', ['/c', 'browserify browserify/site.js | uglifyjs > src/assets/js/main.js']);
+React.stdout.on('data', function (data) {
   console.log(data.toString());
 });
-React.stderr.on('data', (data) => {
+React.stderr.on('data', function (data) {
   console.log(data.toString());
 });
-React.on('exit', (code) => {
-  console.log(`ReactJS built.`);
+React.on('exit', function (code) {
+  console.log('ReactJS built.');
 });
-Script.stdout.on('data', (data) => {
+Script.stdout.on('data', function (data) {
   console.log(data.toString());
 });
-Script.stderr.on('data', (data) => {
+Script.stderr.on('data', function (data) {
   console.log(data.toString());
 });
-Script.on('exit', (code) => {
-  console.log(`Site scripts built.`);
+Script.on('exit', function (code) {
+  console.log('Site scripts built.');
 });
